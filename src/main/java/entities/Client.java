40758dfcf562ebdb39005d9a29d54f475607694c
George <georgeneves.dev@gmail.com>
2023-10-24 12:46:43 -0300

    feat: adicionando entidade Client

---
+++ src/main/java/entities/Client.java
@@ -0,0 +1,29 @@
+package entities;
+
+import lombok.Data;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Data
+public class Client {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private String name;
+    private String email;
+    private Date birthDate;
+
+    public Client(){}
+
+    public Client(String name, String email, Date birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + sdf.format(birthDate) + ") - " + email ;
+    }
+}
