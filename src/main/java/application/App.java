b8c6c1209fa38556f2eb6bee8aa515de02e1dbcb
George <georgeneves.dev@gmail.com>
2023-10-24 12:00:43 -0300

    feat: adicionando locale e scanner

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -3,12 +3,21 @@ package application;
 import entities.OrderItem;
 import entities.Product;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
         Product p = new Product("TV", 1000.0);
         OrderItem oi1 = new OrderItem(1, 1000.0, p);
 
         System.out.println(oi1);
+
+        sc.close();
     }
 }
